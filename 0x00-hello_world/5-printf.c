74f3ab38cebce29963011a25865ffe1cc10e56ee
Mogomotsi45 <Brianmolubi10@gmail.com>
2023-07-17 04:17:34 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that shows print using the print function 
+ * return: o (correct)
+ */
+
+int main(void)
+{
+	printf("with a proper grammer but out comes must be art,\n")
+	return (0);
+}	
